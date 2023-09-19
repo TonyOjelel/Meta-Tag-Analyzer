2d286b2591eca47a1fb1fc5b1ae5cb310b651994
Tony Ojelel <tonyojelel@gmail.com>
2023-09-19 15:44:11 +0300

    1st commit

---
+++ app.js
@@ -0,0 +1,44 @@
+document.getElementById('urlForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const websiteUrl = document.getElementById('websiteUrl').value;
+
+    fetch(websiteUrl)
+        .then(response => response.text())
+        .then(html => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(html, 'text/html');
+
+            const metaTags = doc.querySelectorAll('meta');
+            const metaAnalyzer = document.getElementById('metaAnalyzer');
+            metaAnalyzer.innerHTML = '';
+
+            if (metaTags.length === 0) {
+                metaAnalyzer.innerHTML = '<p>No meta tags found on the page.</p>';
+                return;
+            }
+
+            const table = document.createElement('table');
+            const tableHeader = table.createTHead();
+            const headerRow = tableHeader.insertRow();
+            const cell1 = headerRow.insertCell(0);
+            const cell2 = headerRow.insertCell(1);
+            cell1.innerHTML = '<b>Name</b>';
+            cell2.innerHTML = '<b>Content</b>';
+
+            const tableBody = table.createTBody();
+            metaTags.forEach((tag, index) => {
+                const name = tag.getAttribute('name') || tag.getAttribute('property') || tag.getAttribute('itemprop') || '';
+                const content = tag.getAttribute('content') || '';
+                const row = tableBody.insertRow();
+                row.insertCell(0).textContent = name;
+                row.insertCell(1).textContent = content;
+            });
+
+            metaAnalyzer.appendChild(table);
+        })
+        .catch(error => {
+            console.error(error);
+            alert('An error occurred while fetching and analyzing the website. Please try again.');
+        });
+});
